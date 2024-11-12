0cf827b1f0281a72fb28ca3711947fd4864ec9e3
czajb <74600853+czajb@users.noreply.github.com>
2024-11-12 15:33:23 +0100

    Update docker-install.ps1

--- docker-install.ps1
+++ docker-install.ps1
@@ -1,1 +1,25 @@
-#test
+#Install Docker Destop
+# Check if the script is run as an administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Host "Please run this script with administrator privileges!"
+    exit
+}
+ 
+# Settings
+$dockerInstallerUrl = "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+$installerPath = "$env:TEMP\DockerDesktopInstaller.exe"
+ 
+# Download Docker Desktop installer
+Write-Host "Downloading Docker Desktop..."
+Invoke-WebRequest -Uri $dockerInstallerUrl -OutFile $installerPath
+ 
+# Install Docker Desktop
+Write-Host "Installing Docker Desktop..."
+Start-Process -FilePath $installerPath -ArgumentList "install --quiet" -Wait
+ 
+# Clean up installer after completion
+Write-Host "Cleaning up..."
+Remove-Item $installerPath
+ 
+Write-Host "Docker Desktop installation complete."
